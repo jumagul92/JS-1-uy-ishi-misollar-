e447dcfafcf7fb082507f0c476b257a3ba9e62d5
Jumagul <umarovajumagul76@gmail.com>
2023-10-10 08:24:12 +0100

    create html

---
+++ script.js
@@ -0,0 +1,76 @@
+let name = prompt("Ismingizni kiriting")
+console.log(`Sizning ismingiz ${name}`);
+let age = +prompt("Yoshingizni kiriting")
+
+console.log(`Sizning yoshingiz ${age} da`);
+
+let task1 = prompt("Misolni yeching: 6+8=?")
+if (task1==(6+8)) {
+    console.log(`Sizning javobingiz to'g'ri: ${task1}`);
+    
+} else{
+    console.log(`Sizning javobingiz noto'g'ri: ${task1}. To'g'ri javob: ${6+8}`);
+}
+
+let task2 = +prompt("Misolni yeching: 7*6=?")
+if (task2==(7*6)) {
+    console.log(`Sizning javobingiz to'g'ri: ${task2}`);
+} else{
+    console.log(`Sizning javobingiz noto'g'ri: ${task2}. To'g'ri javob: ${7*6}`);
+}
+
+let task3 = +prompt("Misolni yeching: 17-6=?")
+if (task3==(17-6)) {
+    console.log(`Sizning javobingiz to'g'ri: ${task3}`);
+}else {
+    console.log(`Sizning javobingiz noto'g'ri: ${task3}. To'g'ri javob: ${17-6}`);
+}
+
+let task4 = +prompt("Misolni yeching: 25/5=?")
+if (task4==(25/5)) {
+    console.log(`Sizning javobingiz to'g'ri: ${task4}`);
+} else{
+    console.log(`Sizning javobingiz noto'g'ri: ${task4}. To'g'ri javob: ${25/5}`);
+}
+
+let task5 = +prompt("Misolni yeching: 26%4=?")
+if (task5==(26%4)) {
+    console.log(`Sizning javobingiz to'g'ri: ${task5}`);
+} else{
+    console.log(`Sizning javobingiz noto'g'ri: ${task5}. To'g'ri javob: ${26%4}`);
+}
+
+let task6 = +prompt("Misolni yeching:14+8=?")
+if (task6==(14+8)) {
+    console.log(`Sizning javobingiz to'g'ri: ${task6}`);
+} else{
+    console.log(`Sizning javobingiz noto'g'ri: ${task6}. To'g'ri javob: ${14+8}`);
+}
+
+let task7 = +prompt("Misolni yeching: 9*7=?")
+if(task7==(9*7)){
+    console.log(`Sizning javobingiz to'g'ri: ${task7}`);
+} else{
+    console.log(`Sizning javobingiz noto'g'ri: ${task7}. To'g'ri javob: ${9*7}`);
+}
+
+let task8 = +prompt("Misolni yeching: 19-10=?")
+if (task8==(19-10)) {
+    console.log(`Sizning javobingiz to'g'ri: ${task8}`);
+}else{
+    console.log(`Sizning javobingiz noto'g'ri: ${task8}. To'g'ri javob: ${19-10}`);
+}
+
+let task9 = +prompt("Misolni yeching: 36/9=?")
+if (task9==(36/9)) {
+    console.log(`Sizning javobingiz to'g'ri: ${task9}`);
+} else{
+    console.log(`Sizning javobingiz noto'g'ri: ${task9}. To'g'ri javob: ${36/9}`);
+}
+
+let task10 = +prompt("Misolni yeching: 27%8=?")
+if (task10==(27%8)) {
+    console.log(`Sizning javobingiz to'g'ri: ${task10}`);
+} else{
+    console.log(`Sizning javobingiz noto'g'ri: ${27%8}. To'g'ri javob: ${27%8}`);
+}
